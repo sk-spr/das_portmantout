6d6beb94b819b5458ab6c1e7e8f0535c6491f02b
Skye Sprung <skye.sprung@student.tugraz.at>
2023-01-12 16:29:25 +0100

    added basic cleaning

--- cleaner/DataCleaner.cs
+++ cleaner/DataCleaner.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Welcome to PortManTout cleaner.\nCopyright Skye Sprung 2023");
+
+using var reader = new StreamReader(File.OpenRead("../data/raw.txt"));
+List<string> lines = new();
+while (!reader.EndOfStream)
+    lines.Add(reader.ReadLine() ?? "");
+Console.WriteLine("Done reading.");
+reader.Close();
+List<string> words = new();
+foreach (var line in lines)
+{
+    //remove variable whitespace
+    var singleWhiteSpace = Regex.Replace(line, "[ +\t+]", " ");
+    try
+    {
+        var parts = singleWhiteSpace.Split(" ");
+        if (parts.Length == 3)
+        {
+            var readWord = parts[1];
+            var frequency = int.Parse(parts[2]);
+            if(!Regex.IsMatch(readWord, "[^a-zA-ZüÜäÄöÖß']"))
+                words.Add(readWord);
+            
+            //words.Add(new Entry { word = readWord, occurrances = frequency });
+        }
+
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"Exception encountered on line {line}: {e.Message}. Pre split is {singleWhiteSpace}. Attempting to continue and ignore.");
+    }
+}
+
+using var outstream = File.OpenWrite("../data/processed.json");
+JsonSerializer.Serialize(outstream, words);
